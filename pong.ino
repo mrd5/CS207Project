70b46b30d172b6cddf3754192bb3278b3e8cc29a
mrd5 <32419148+mrd5@users.noreply.github.com>
2017-10-29 12:36:03 -0600

    Create pong.ino
    I've started my project! I first describe what the program does, and the libraries needed. I've also provided a link for the libraries, since they are not included with arduino. Next I will be defining the variables needed for the program.

---
+++ pong.ino
@@ -0,0 +1,20 @@
+/*
+ * Pong game on the Arduino
+ * Based on James Bruce's version found here: 
+ * http://www.makeuseof.com/tag/how-to-recreate-the-classic-pong-game-using-arduino/
+ * enhanced, by Mathew Dorish
+ * Uses wither photoresistors or temperature sensors as controllers! 
+ * Plays on a TV.
+ */
+
+//Required arduino libraries
+#include <TVout.h>
+//Download TVout here: https://code.google.com/archive/p/arduino-tvout/downloads
+//Download the TVoutBeta1.zip and extract it. Place the three folders in it in your Arduino libraries folder. 
+#include <fontALL.h>
+//fontALL.h is included in the above download
+
+//VARIABLES
+
+
+	
